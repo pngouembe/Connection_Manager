40847c2159b5b0f6df650634e6292733c7a09fcc
Paul Ngouembe <paul.ngouembe@gmail.com>
2021-07-27 20:02:06 +0200

    Added CLI parameter support + usage message

--- src/Connection_manager.py
+++ src/Connection_manager.py
@@ -3,14 +3,27 @@
 import os
 import socket
 import sys
+import getopt
 
+def usage():
+    print("--------------Connection_manager--------------")
 
-HOST = 'localhost'
-PORT = 65432
+    print("""Brief: Program designed to keep track of
+availability of a remote machine""")
+    print("""python3 Connection_manager.py -a <IP address> -p <Port> -c <"Command">""")
+    print("""Parameters:
+    # for clients
+        -a, --addr: remote machine's IP address
+        -p, --port: port used for the connection
+        -c, --cmd : command used for the connection
+    # for server
+        --server : Run as server
+        -a, --addr: machine's IP address
+        -p, --port: port used for the connection""")
 
-def launchServer():
+def launchServer(host, port, cmd):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
+    s.bind((host, port))
     s.listen(1)
     conn, addr = s.accept()
     print('Connected by', addr)
@@ -20,32 +33,49 @@ def launchServer():
             break
         conn.sendall(data)
 
-def launchClient():
+def launchClient(host, port, cmd):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
+    s.connect((host, port))
     s.sendall(b'Hello, world')
     data = s.recv(1024)
 
     print('Received', repr(data))
+    if cmd != '':
+        print("Launching cmd : {}".format(cmd))
+        os.system(cmd)
 
 def main(argv):
+    
+    host = 'localhost'
+    port = 65432
+    cmd = ''
     print("launching connection manager")
     try:
-        opts, args = getopt.getopt(argv, "a:p:", "addr=port=")
+        opts, args = getopt.getopt(argv, "a:p:c:h", ["addr=","port=","cmd=","server", "help"])
       
-    except:
-        print("Error")
+    except getopt.GetoptError as e:
+        print("Error : {}".format(e.msg))
+        usage()
+        sys.exit(2)
   
     for opt, arg in opts:
         if opt in ['-a', 'addr']:
-            HOST = arg
+            host = arg
         elif opt in ['-p', 'port']:
-            PORT = arg
+            port = arg
+        elif opt in ['-c', 'cmd']:
+            cmd = arg
+        elif opt in ['-h', 'help']:
+            usage()
+            sys.exit(0)
+            
 
     if "--server" in argv :
-        launchServer()
+        launchServer(host, port, cmd)
+        print ("server exit")
     else:
-        launchClient()
+        launchClient(host, port, cmd)
+        print ("client exit")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
