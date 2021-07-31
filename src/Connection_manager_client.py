6c164b43e1f0c5859c31ca349dc227ee0da33e72
Ana Daouda <anadaouda@gmail.com>
2021-07-31 12:34:37 +0200

    Separation of client and server code

---
+++ src/Connection_manager_client.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import os
+import socket
+import sys
+import getopt
+
+def usage():
+    print("--------------Connection_manager--------------")
+
+    print("""Brief: Program designed to keep track of
+availability of a remote machine""")
+    print("""python3 Connection_manager.py -a <IP address> -p <Port> -c <"Command">""")
+    print("""Parameters:
+        -a, --addr: remote machine's IP address
+        -p, --port: port used for the connection
+        -c, --cmd : command used for the connection""")
+
+def launchClient(host, port, cmd):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+    print('Received', repr(data))
+    if cmd != '':
+        print("Launching cmd : {}".format(cmd))
+        os.system(cmd)
+
+def main(argv):
+    
+    host = 'localhost'
+    port = 65432
+    cmd = ''
+    print("launching connection manager")
+    try:
+        opts, args = getopt.getopt(argv, "a:p:c:h", ["addr=","port=","cmd=","server", "help"])
+      
+    except getopt.GetoptError as e:
+        print("Error : {}".format(e.msg))
+        usage()
+        sys.exit(2)
+  
+    for opt, arg in opts:
+        if opt in ['-a', 'addr']:
+            host = arg
+        elif opt in ['-p', 'port']:
+            port = arg
+        elif opt in ['-c', 'cmd']:
+            cmd = arg
+        elif opt in ['-h', 'help']:
+            usage()
+            sys.exit(0)
+            
+    launchClient(host, port, cmd)
+    print ("client exit")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- src/Connection_manager_server.py
+++ src/Connection_manager_server.py
@@ -12,12 +12,6 @@ def usage():
 availability of a remote machine""")
     print("""python3 Connection_manager.py -a <IP address> -p <Port> -c <"Command">""")
     print("""Parameters:
-    # for clients
-        -a, --addr: remote machine's IP address
-        -p, --port: port used for the connection
-        -c, --cmd : command used for the connection
-    # for server
-        --server : Run as server
         -a, --addr: machine's IP address
         -p, --port: port used for the connection""")
 
@@ -33,17 +27,6 @@ def launchServer(host, port, cmd):
             break
         conn.sendall(data)
 
-def launchClient(host, port, cmd):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-    print('Received', repr(data))
-    if cmd != '':
-        print("Launching cmd : {}".format(cmd))
-        os.system(cmd)
-
 def main(argv):
     
     host = 'localhost'
@@ -51,7 +34,7 @@ def main(argv):
     cmd = ''
     print("launching connection manager")
     try:
-        opts, args = getopt.getopt(argv, "a:p:c:h", ["addr=","port=","cmd=","server", "help"])
+        opts, args = getopt.getopt(argv, "a:p:h", ["addr=","port=","server", "help"])
       
     except getopt.GetoptError as e:
         print("Error : {}".format(e.msg))
@@ -63,19 +46,12 @@ def main(argv):
             host = arg
         elif opt in ['-p', 'port']:
             port = arg
-        elif opt in ['-c', 'cmd']:
-            cmd = arg
         elif opt in ['-h', 'help']:
             usage()
             sys.exit(0)
-            
-
-    if "--server" in argv :
-        launchServer(host, port, cmd)
-        print ("server exit")
-    else:
-        launchClient(host, port, cmd)
-        print ("client exit")
+    
+    launchServer(host, port, cmd)
+    print ("server exit")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
