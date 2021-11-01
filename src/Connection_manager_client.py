9a67351c99ea5396c02355a26495ae1ccf440013
Paul Ngouembe <paul.ngouembe@gmail.com>
2021-11-01 21:47:25 +0100

    Adding config file template for the client

--- src/Connection_manager_client.py
+++ src/Connection_manager_client.py
@@ -1,57 +1,90 @@
 #!/usr/bin/env python3
 
+from typing import Tuple
+import yaml
+import getpass
+import time
+import argparse
+import os
 import socket
 import sys
-from typing import Tuple
+
 sys.path.append('../modules')
+
+# Project modules
 from display import Logger, LoggerMgr
 from user import User
 from com_protocole import ComProtocole, ComHeaders
 import client_actions
 import global_vars
-import argparse
-import time
-import signal
-import getpass
 
 log_mgr = LoggerMgr()
 log_mgr.launch_logger_mgr()
-Log:Logger = log_mgr.Loggers[0]
+Log: Logger = log_mgr.Loggers[0]
 
-private_info = ["address", "port", "cmd"]
+private_info = ["address", "port", "cmd", "cfg_file", "Entity_type"]
+default_config_file_name = os.path.join(
+    os.path.realpath(os.path.dirname(__file__)),
+    "../config/client_config_template.yml")
+
 
 def setup_argument_parser():
-    parser = argparse.ArgumentParser(prog='Client-side connection manager',\
-                                     usage='python3 Connection_manager_client.py -a <IP address> -p <Port> -c \'<Command>\'',\
+    parser = argparse.ArgumentParser(prog='Client-side connection manager',
+                                     usage='python3 Connection_manager_client.py -a <IP address> -p <Port> -c \'<Command>\'',
                                      description='Program designed to keep track of the availability of a remote machine')
 
     # metavar='' so that there is no all caps argument name in usage
-    parser.add_argument('-n', '--name', default=getpass.getuser(), metavar='', help="user's name that will be communicated to the server")
-    parser.add_argument('-a', '--address', default='localhost', metavar='', help='the machine\'s IP address')
-    parser.add_argument('-p', '--port', type=int, default=65432, metavar='', help='port used for the connection')
-    parser.add_argument('-c', '--cmd', default='', metavar='', help='command used for the connection')
-    parser.add_argument('-t', '--timeout', type=int, default=0, metavar='', help='timespan before client automatically ends the connection (in sec)')
-    parser.add_argument('-C', '--comment', type=str, default='', metavar='', help='comment to show to other users')
+    parser.add_argument('-n', '--name', metavar='',
+                        help="user's name that will be communicated to the server", required=False)
+    parser.add_argument('-a', '--address', default='localhost',
+                        metavar='', help='the machine\'s IP address')
+    parser.add_argument('-p', '--port', type=int, default=65432,
+                        metavar='', help='port used for the connection')
+    parser.add_argument('-c', '--cmd', default='', metavar='',
+                        help='command used for the connection')
+    parser.add_argument('-t', '--timeout', type=int, default=0, metavar='',
+                        help='timespan before client automatically ends the connection (in sec)')
+    parser.add_argument('-C', '--comment', type=str, default='',
+                        metavar='', help='comment to show to other users')
+    parser.add_argument('--cfg_file',
+                        type=str,
+                        default=default_config_file_name,
+                        metavar='',
+                        help='path to the config file',
+                        required=False)
 
     return parser
 
-def get_arguments(parser) -> Tuple[dict, dict]:
+
+def get_config(parser) -> Tuple[dict, dict]:
     args = parser.parse_args()
-    dict = vars(args)
-    public_dict = {k:dict[k] for k in dict.keys() if k not in private_info}
-    private_dict = {k:dict[k] for k in dict.keys() if k in private_info}
+    cfg_dict = {}
+    if os.path.isfile(args.cfg_file):
+        with open(args.cfg_file, "r") as f:
+            cfg_dict: dict = yaml.load(f)
+        for key, value in vars(args).items():
+            if value:
+                cfg_dict[key] = value
+    else:
+        cfg_dict = vars(args)
+
+    print(cfg_dict)
+    public_dict = {k: cfg_dict[k] for k in cfg_dict.keys() if k not in private_info}
+    private_dict = {k: cfg_dict[k] for k in cfg_dict.keys() if k in private_info}
     return (public_dict, private_dict)
+
 
 def stop_client(*args):
     Log.log(Log.warn_level, "Timeout reached, stoping the client")
     raise KeyboardInterrupt
+
 
 def launchClient(user: User) -> None:
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((user.get_user_info("address"), user.get_user_info("port")))
 
-    #dumping user's public info to send them to the server
+    # dumping user's public info to send them to the server
     payload = user.json_dump()
     msg = ComProtocole.generate_msg(ComHeaders.INTRODUCE, payload)
     Log.log(Log.info_level, "Sending: {}".format(payload))
@@ -81,9 +114,10 @@ def launchClient(user: User) -> None:
     s.close()
     del user
 
+
 def main(argv):
     parser = setup_argument_parser()
-    public_dict, private_dict = get_arguments(parser)
+    public_dict, private_dict = get_config(parser)
 
     # Adding client parameters into user object
     user = User(public_dict)
@@ -102,6 +136,7 @@ def main(argv):
     Log.log(Log.info_level, "client exit")
     time.sleep(1)
     log_mgr.stop_logger_mgr()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- src/Connection_manager_server.py
+++ src/Connection_manager_server.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 
+import argparse
 from socket import socket, MSG_DONTWAIT, AF_INET, SOCK_STREAM
 import threading
 import time
 import sys
+
 sys.path.append('../modules')
-from display import Logger, LoggerMgr
-from user import User
-from com_protocole import ComProtocole, ComHeaders
-import server_actions
-import argparse
+
+# Project modules
 import global_vars
+import server_actions
+from com_protocole import ComProtocole, ComHeaders
+from user import User
+from display import Logger, LoggerMgr
 
 log_mgr = LoggerMgr()
 log_mgr.launch_logger_mgr()
-Log:Logger = log_mgr.Loggers[0]
+Log: Logger = log_mgr.Loggers[0]
 
 c_list_lock = threading.Lock()
 connection_list = []
@@ -22,33 +25,39 @@ connection_number = 0
 active_connection = 0
 
 
-
 def setup_argument_parser():
-    parser = argparse.ArgumentParser(prog='Server-side connection manager',\
-                                     usage='python3 Connection_manager_server.py -a <IP address> -p <Port>',\
+    parser = argparse.ArgumentParser(prog='Server-side connection manager',
+                                     usage='python3 Connection_manager_server.py -a <IP address> -p <Port>',
                                      description='Program designed to keep track of the availability of a remote machine')
 
     # metavar='' so that there is no all caps argument name in usage
-    parser.add_argument('-a', '--address', default='localhost', metavar='', help='the machine\'s IP address')
-    parser.add_argument('-p', '--port', type=int, default=65432, metavar='', help='port used for the connection')
-    parser.add_argument('-t', '--timeout', type=float, default=0, metavar='', help='Maximum time allowed to a client')
+    parser.add_argument('-a', '--address', default='localhost',
+                        metavar='', help='the machine\'s IP address')
+    parser.add_argument('-p', '--port', type=int, default=65432,
+                        metavar='', help='port used for the connection')
+    parser.add_argument('-t', '--timeout', type=float, default=0,
+                        metavar='', help='Maximum time allowed to a client')
 
     return parser
+
 
 def get_arguments(parser):
     args = parser.parse_args()
     return vars(args)
 
-def client_handler(user:User):
 
-    Log:Logger = user.get_logger()
-    Log.log(Log.info_level, "Active connections : {}".format(user.get_user_count()))
-    client:socket = user.get_user_info("socket_obj")
+def client_handler(user: User):
+
+    Log: Logger = user.get_logger()
+    Log.log(Log.info_level, "Active connections : {}".format(
+        user.get_user_count()))
+    client: socket = user.get_user_info("socket_obj")
     while user.is_com_active():
         try:
             data = client.recv(1024)
         except ConnectionResetError:
-            Log.log(Log.err_level, "Connection to {} lost".format(user.get_user_name()))
+            Log.log(Log.err_level, "Connection to {} lost".format(
+                user.get_user_name()))
             break
         if not data:
             break
@@ -60,10 +69,12 @@ def client_handler(user:User):
 
     client.close()
     if user.is_duplicate():
-        Log.log(Log.dbg_level, "Closing {} duplicate".format(user.get_user_name()))
+        Log.log(Log.dbg_level, "Closing {} duplicate".format(
+            user.get_user_name()))
     else:
         Log.log(Log.info_level, "{} disconnected".format(user.get_user_name()))
-        Log.log(Log.info_level, "Active connections : {}".format(user.get_user_count() - 1))
+        Log.log(Log.info_level, "Active connections : {}".format(
+            user.get_user_count() - 1))
     user.remove_from_list()
     Log.log(Log.dbg_level, "Client list {}".format(user.get_user_names_list()))
     del user
@@ -88,9 +99,10 @@ def launchServer(server: User):
         except KeyboardInterrupt:
             break
         user = User()
-        user.add_private_info({"socket_obj": conn,"ip_addr":addr})
+        user.add_private_info({"socket_obj": conn, "ip_addr": addr})
         user.register_logger(Log)
-        Log.log(Log.info_level, "addr: {}".format(user.get_user_info("ip_addr")))
+        Log.log(Log.info_level, "addr: {}".format(
+            user.get_user_info("ip_addr")))
         t = threading.Thread(target=client_handler, args=(user,))
         t.start()
         del user
@@ -99,6 +111,7 @@ def launchServer(server: User):
     except:
         pass
     s.close()
+
 
 def main(argv):
     parser = setup_argument_parser()
@@ -111,12 +124,14 @@ def main(argv):
     global_vars.init()
 
     Log.log(Log.info_level, "launching connection manager")
-    Log.log(Log.info_level, "Max client timeout : {}s".format(server.get_user_info("timeout")))
+    Log.log(Log.info_level, "Max client timeout : {}s".format(
+        server.get_user_info("timeout")))
     global_vars.max_client_execution_time = server.get_user_info("timeout")
     launchServer(server)
     Log.log(Log.info_level, "server exit")
     time.sleep(1)
     log_mgr.stop_logger_mgr()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
